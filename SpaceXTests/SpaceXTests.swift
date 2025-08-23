ba923008272f140ddb78fc9b60763ecbff3a7efb
anghel.elena <anghel.elena@icloud.com>
2025-08-23 18:06:56 +0300

    Initial Commit

---
+++ SpaceXTests/SpaceXTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpaceXTests.swift
+//  SpaceXTests
+//
+//  Created by Elena Anghel on 23.08.2025.
+//
+
+import Testing
+
+struct SpaceXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
